e43bc9ad10e6e71867dc3b3378625a483474566b
Paul Lapey <plapey45@gmail.com>
2026-09-20 17:11:18 -0700

    drop keybindings that were duplicated with sxhkd

--- config.h
+++ config.h
@@ -91,18 +91,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *bookmarkscmd[] = { "/home/paul/scripts/web_bookmarks.sh", NULL };
-static const char *newtabbookmarkscmd[] = { "/home/paul/scripts/web_bookmarks.sh", "-n", NULL };
-
-/* volume stuff */
-/* static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL }; */
-/* static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL }; */
-/* static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL }; */
-
-static const char *upvol[]   = { "/usr/bin/pulsemixer", "--change-volume", "+5",     NULL };
-static const char *downvol[]   = { "/usr/bin/pulsemixer", "--change-volume", "-5",     NULL };
-static const char *mutevol[] = { "/usr/bin/pulsemixer", "--toggle-mute", NULL };
-
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -147,14 +135,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-
-	// Volume stuff
-	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	//Custom Commanbds	
-	{ MODKEY,                       XK_b,      spawn,          {.v = bookmarkscmd } },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = newtabbookmarkscmd } },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
